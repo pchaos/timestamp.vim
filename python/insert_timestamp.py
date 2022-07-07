a02def6b188d7c13bd202a523bda562f7403e6e6
pchaos <drifthua@gmail.com>
2022-07-07 12:14:38 +0800

    update python

--- python/insert_timestamp.py
+++ python/insert_timestamp.py
@@ -162,7 +162,7 @@ def substitute(string, pattern, replace, err_return=''):
     try:
         # print(f"{astr},{pattern},{replace},{err_return}")
         # rv = re.sub(pattern, replace, astr)
-        if re.search(pattern, "Modified"):
+        if re.search("Modified", pattern):
             pattern = "((Last ([cC]hanged?|modified)|Modified)\s*:\s+)\d{4}-\d{2}-\d{2}(\s*)?\d{2}:\d{2}:\d{2}(\s*)?|TIMESTAMP"
         # print(f"exceptipn:{astr},{patttern},{replace},{err_return}")
         rv = re.sub(pattern, replace, string)
